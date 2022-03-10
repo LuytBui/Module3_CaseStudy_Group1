8d8470da7224cbb05e273600246a02285708d245
cuongng22 <theendvg2003@gmail.com>
2022-03-10 14:13:40 +0700

    add

--- src/main/java/com/codegym/controller/CategoryServlet.java
+++ src/main/java/com/codegym/controller/CategoryServlet.java
@@ -1,15 +1,33 @@
 package com.codegym.controller;
 
+import com.codegym.dao.category.ICategoryDAO;
+import com.codegym.model.Category;
+import com.codegym.service.category.ICategoryService;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(name = "CategoryServlet", value = "/categories")
 public class CategoryServlet extends HttpServlet {
+    private ICategoryService categoryService;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        switch (action) {
+            default:{
+                List<Category> categories = categoryService.findAll();
+                request.setAttribute("categories", categories);
+                RequestDispatcher dispatcher = request.getRequestDispatcher("/category/create.jsp");
+                dispatcher.forward(request, response);
+            }
+        }
     }
 
     @Override
