a222c4a39393d2c6ff4cd2ee8c87150f93e225b0
Toan-246 <n.v.toan687@gmail.com>
2022-03-10 09:43:06 +0700

    them file blog/list

--- src/main/java/com/codegym/service/blog/BlogService.java
+++ src/main/java/com/codegym/service/blog/BlogService.java
@@ -1,32 +1,40 @@
 package com.codegym.service.blog;
 
+import com.codegym.dao.blog.IBlogDAO;
 import com.codegym.model.Blog;
+import com.codegym.service.IGeneralService;
 
 import java.util.List;
 
 public class BlogService implements IBlogService{
+    IBlogDAO blogDAO;
+
+    public BlogService(IBlogDAO blogDAO) {
+        this.blogDAO = blogDAO;
+    }
+
     @Override
     public List<Blog> findAll() {
-        return null;
+        return blogDAO.findAll();
     }
 
     @Override
     public Blog findByID(int id) {
-        return null;
+        return blogDAO.findByID(id);
     }
 
     @Override
     public boolean create(Blog blog) {
-        return false;
+        return blogDAO.create(blog);
     }
 
     @Override
     public boolean deleteById(int id) {
-        return false;
+        return blogDAO.deleteById(id);
     }
 
     @Override
     public boolean updateById(int id, Blog blog) {
-        return false;
+        return blogDAO.updateById(id, blog);
     }
 }
