c6a24ff7f0f17460bf1c5ee86e19a2b4343cbe12
LuytBui <luytbq43@gmail.com>
2022-03-10 14:14:30 +0700

    review User service, DAO

--- src/main/java/com/codegym/dao/user/UserDAO.java
+++ src/main/java/com/codegym/dao/user/UserDAO.java
@@ -12,16 +12,18 @@ import java.util.Date;
 import java.util.List;
 
 public class UserDAO implements IUserDAO {
-    public static final String SELECT_FROM_ALL_USER = "SELECT * FROM user ";
-    public static final String SELECT_FROM_ONE_USER = "SELECT * from user where  id = ?";
-    public static final String INSERT_USER = "INSERT into user (username,passwork,phone,email,dateOfBirth,gender,address,status,role_id) value (?,?,?,?,?,?,?,?,?)";
+    public static final String SQL_SELECT_ALL_USER = "SELECT * FROM user ";
+    public static final String SQL_SELECT_USER_BY_ID = "SELECT * from user where  id = ?";
+    public static final String SQL_INSERT_USER = "INSERT into user (username,passwork,phone,email,dateOfBirth,gender,address,status,role_id) value (?,?,?,?,?,?,?,?,?)";
+    public static final String SQL_UPDATE_USER_BY_ID = "update user set username =?,password =?,phone =?,email =?,dateOfBirth =?,gender=?,address = ?,status =? , role_id =?";
+    public static final String SQL_DELETE_USER_BY_ID = "delete  from user where  id = ?";
     private Connection connection = DBConnection.getConnection();
 
     @Override
     public List<User> findAll() {
         List<User> users = new ArrayList<>();
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_FROM_ALL_USER);
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT_ALL_USER);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
@@ -49,7 +51,7 @@ public class UserDAO implements IUserDAO {
     public User findByID(int id) {
         User user = new User();
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_FROM_ONE_USER);
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_SELECT_USER_BY_ID);
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
@@ -74,7 +76,7 @@ public class UserDAO implements IUserDAO {
     @Override
     public boolean create(User user) {
         try {
-            PreparedStatement preparedStatement =connection.prepareStatement(INSERT_USER);
+            PreparedStatement preparedStatement =connection.prepareStatement(SQL_INSERT_USER);
             preparedStatement.setString(1,user.getUsername());
             preparedStatement.setString(2,user.getPassword());
             preparedStatement.setString(3,user.getPhone());
@@ -93,7 +95,7 @@ public class UserDAO implements IUserDAO {
     @Override
     public boolean deleteById(int id) {
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("delete  from user where  id = ?");
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL_DELETE_USER_BY_ID);
             preparedStatement.setInt(1,id);
             return preparedStatement.executeUpdate() >0;
         } catch (SQLException e) {
@@ -106,7 +108,7 @@ public class UserDAO implements IUserDAO {
     public boolean updateById(int id, User user) {
         PreparedStatement preparedStatement = null;
         try {
-            preparedStatement = connection.prepareStatement("update user set username =?,password =?,phone =?,email =?,dateOfBirth =?,gender=?,address = ?,status =? , role_id =?");
+            preparedStatement = connection.prepareStatement(SQL_UPDATE_USER_BY_ID);
             preparedStatement.setString(1,user.getUsername());
             preparedStatement.setString(2,user.getPassword());
             preparedStatement.setString(3,user.getPhone());
