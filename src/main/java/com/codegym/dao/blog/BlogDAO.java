707d0a793d2d7556af7242d7bf6df42c2890fbfc
Toan-246 <n.v.toan687@gmail.com>
2022-03-10 09:08:35 +0700

    tao DBconnection

--- src/main/java/com/codegym/dao/blog/BlogDAO.java
+++ src/main/java/com/codegym/dao/blog/BlogDAO.java
@@ -1,32 +1,106 @@
 package com.codegym.dao.blog;
 
+import com.codegym.dao.DBConnection;
 import com.codegym.model.Blog;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class BlogDAO implements IBlogDAO{
+public class BlogDAO implements IBlogDAO {
+    Connection connection = DBConnection.getConnection();
+
     @Override
     public List<Blog> findAll() {
-        return null;
+        List<Blog> blogs = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM blogs");
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                int category_id = rs.getInt("category_id");
+                int user_id = rs.getInt("user_id");
+                String tittle = rs.getString("tittle");
+                String content = rs.getString("content");
+                String dateModified = rs.getString("dateModified");
+
+                Blog blog = new Blog(id, category_id, user_id, tittle, content, dateModified);
+                blogs.add(blog);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return blogs;
     }
 
     @Override
     public Blog findByID(int id) {
-        return null;
+        Blog blog = new Blog();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM blogs where id = ?");
+            preparedStatement.setInt(1, id);
+            ResultSet rs = preparedStatement.executeQuery();
+            while (rs.next()) {
+                int category_id = rs.getInt("category_id");
+                int user_id = rs.getInt("user_id");
+                String tittle = rs.getString("tittle");
+                String content = rs.getString("content");
+                String dateModified = rs.getString("dateModified");
+                blog = new Blog(id, category_id, user_id, tittle, content, dateModified);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return blog;
     }
 
     @Override
     public boolean create(Blog blog) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO blogs (category_id, user_id, tittle, content, dateModified) values  (?, ?, ?, ?, ?)");
+            preparedStatement.setInt(1, blog.getCategory_id());
+            preparedStatement.setInt(2, blog.getUser_id());
+            preparedStatement.setString(3, blog.getTittle());
+            preparedStatement.setString(4, blog.getContent());
+            preparedStatement.setString(5, blog.getDateModified());
+            return preparedStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public boolean deleteById(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE from blogs where  id = ?");
+            preparedStatement.setInt(1, id);
+            return preparedStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     @Override
     public boolean updateById(int id, Blog blog) {
+        List<Blog> blogs = new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE blogs set category_id = ?, user_id = ?, tittle = ?, content = ?, dateModified = ? where id = ?");
+            preparedStatement.setInt(1, blog.getCategory_id());
+            preparedStatement.setInt(2, blog.getUser_id());
+            preparedStatement.setString(3, blog.getTittle());
+            preparedStatement.setString(4, blog.getContent());
+            preparedStatement.setString(5, blog.getDateModified());
+            preparedStatement.setInt(6, blog.getId());
+            return preparedStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 }
--- src/main/java/com/codegym/model/Blog.java
+++ src/main/java/com/codegym/model/Blog.java
@@ -1,4 +1,83 @@
 package com.codegym.model;
 
 public class Blog {
+    private int id;
+
+    private int category_id;
+
+    private int user_id;
+
+    private String tittle;
+
+    private String content;
+
+    private String dateModified;
+
+    public Blog() {
+    }
+
+    public Blog(int category_id, int user_id, String tittle, String content, String dateModified) {
+        this.category_id = category_id;
+        this.user_id = user_id;
+        this.tittle = tittle;
+        this.content = content;
+        this.dateModified = dateModified;
+    }
+
+    public Blog(int id, int category_id, int user_id, String tittle, String content, String dateModified) {
+        this.id = id;
+        this.category_id = category_id;
+        this.user_id = user_id;
+        this.tittle = tittle;
+        this.content = content;
+        this.dateModified = dateModified;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
 }
