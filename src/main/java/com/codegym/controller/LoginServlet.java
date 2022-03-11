6d8bf1600d97260e82eb841a1c00dd3c05e0910b
LuytBui <luytbq43@gmail.com>
2022-03-11 08:33:06 +0700

    phat trien tinh nang login

--- src/main/java/com/codegym/controller/LoginServlet.java
+++ src/main/java/com/codegym/controller/LoginServlet.java
@@ -11,10 +11,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
-@WebServlet(name = "LoginServlet", value = "/login")
+@WebServlet(name = "LoginServlet", value = "")
 public class LoginServlet extends HttpServlet {
 
     UserService userService = new UserService(new UserDAO());
@@ -46,7 +47,11 @@ public class LoginServlet extends HttpServlet {
         // check for password
         loginSuccess = password.equals(user.getPassword());
         if (loginSuccess) {
-            System.out.println("login success");
+
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            response.sendRedirect("/blogs");
+
         } else {
             // Wrong password
             request.setAttribute("message", WRONG_PASSWORD);
